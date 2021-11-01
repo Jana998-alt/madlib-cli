057f6d7dc986dc65a1192b0e225ea300058315e8
Jana Abdelkader <janaosama1300@gmail.com>
2021-11-01 21:50:10 +0200

    made the skeleton

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,34 @@
+
+# welcome message
+
+def welcome_mss():
+    print('Hey there! \nwelcome to madlib game! \nI will ask you a series of quistions, and I\ll ask you to answer them according to the instructions, the result will be something fun I promise. \n let\'s get started....')
+    
+
+# read & parse file
+
+def  read_template():
+    pass
+
+def parse_template():
+    pass
+
+def merge():
+    pass
+
+# prompt input sequence  
+
+def prompt_sequence_of_variables():
+    pass
+
+# - prompt with answer  
+
+def prompt_result_text():
+    pass
+
+#  write the result in a new file
+def write_result_in_file():
+    pass
+
+if __name__ == "__main__":
+    welcome_mss()
